fc84381e1ed485cc8462d50352a4a6222789dca6
Diwan01kawishka <96010085+Diwan01kawishka@users.noreply.github.com>
2022-06-27 09:59:17 +0530

    Update stringSession.js

--- Bug-hole/stringSession.js
+++ Bug-hole/stringSession.js
@@ -26,7 +26,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'BUG;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'AQUA;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
